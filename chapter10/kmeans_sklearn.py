fb5e79b1920966fce5c84264b1437e7c54ba039c
haydenliu <yuxi.liu.ece@gmail.com>
2020-09-07 20:46:28 -0700

    Simply code for cluster visualization in kmeans_sklearn in chapter 10

--- chapter10/kmeans_sklearn.py
+++ chapter10/kmeans_sklearn.py
@@ -11,7 +11,6 @@ iris = datasets.load_iris()
 X = iris.data[:, 2:4]
 y = iris.target
 
-import numpy as np
 from matplotlib import pyplot as plt
 
 k = 3
@@ -21,8 +20,7 @@ kmeans_sk.fit(X)
 clusters_sk = kmeans_sk.labels_
 centroids_sk = kmeans_sk.cluster_centers_
 
-for i in range(k):
-    cluster_i = np.where(clusters_sk == i)
-    plt.scatter(X[cluster_i, 0], X[cluster_i, 1])
+
+plt.scatter(X[:, 0], X[:, 1], c=clusters_sk)
 plt.scatter(centroids_sk[:, 0], centroids_sk[:, 1], marker='*', s=200, c='#050505')
 plt.show()
